4a1758d31db4783353b03af4f36227752d088ba8
Administrator <Administrator@WIN-20170113VKT>
2017-03-02 21:52:12 +0800

    try p4

---
+++ src/p1/p20/P4.java
@@ -0,0 +1,17 @@
+package p1.p20;
+
+public class P4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+		int length1 = nums1.length;
+		int length2 = nums2.length;
+		if(nums1[0] >= nums2[length2 - 1]){
+			return;
+		}
+		return 0D;
+    }
+}
